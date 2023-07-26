74de28cdec6308f7a9cbb75b0b23122147c3bcdf
EUIJOON <euijoonism@gmail.com>
2023-07-27 02:57:14 +0900

    [refactor] Member - 회원가입 시 admin 권한으로 부여되는 것 타입에 따라 권한 분리되도록 수정

--- src/main/java/store/greeting/enums/UserType.java
+++ src/main/java/store/greeting/enums/UserType.java
@@ -1,5 +1,17 @@
 package store.greeting.enums;
 
 public enum UserType {
-  USER, ADMIN
+  USER,
+  ADMIN,
+  ;
+
+  public static UserType from(String type) {
+    if (USER.name().equals(type)) {
+      return USER;
+    } else if (ADMIN.name().equals(type)) {
+      return ADMIN;
+    } else {
+      throw new IllegalStateException();
+    }
+  }
 }
