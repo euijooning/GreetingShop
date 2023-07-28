f6020a0643a8507371f84b17af81670dfa36dddc
EUIJOON <euijoonism@gmail.com>
2023-07-28 23:12:28 +0900

    [refactor] Product - 서버 실행과 동시에 상품 5개 기본으로 등록되도록 변경

--- src/main/java/store/greeting/GreetingApplication.java
+++ src/main/java/store/greeting/GreetingApplication.java
@@ -6,9 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import store.greeting.enums.SellStatus;
 import store.greeting.enums.UserType;
 import store.greeting.member.entity.Member;
 import store.greeting.member.repository.MemberRepository;
+import store.greeting.product.entity.Product;
+import store.greeting.product.entity.ProductImage;
+import store.greeting.product.repository.ProductImageRepository;
+import store.greeting.product.repository.ProductRepository;
 
 @SpringBootApplication
 public class GreetingApplication {
@@ -16,10 +21,16 @@ public class GreetingApplication {
   private final MemberRepository memberRepository;
   private final PasswordEncoder passwordEncoder;
 
+  private final ProductRepository productRepository;
+  private final ProductImageRepository productImageRepository;
+
   @Autowired
-  public GreetingApplication(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
+  public GreetingApplication(MemberRepository memberRepository,
+      PasswordEncoder passwordEncoder, ProductRepository productRepository ,ProductImageRepository productImageRepository) {
     this.memberRepository = memberRepository;
     this.passwordEncoder = passwordEncoder;
+    this.productRepository = productRepository;
+    this.productImageRepository = productImageRepository;
   }
 
   @Bean
@@ -46,6 +57,23 @@ public class GreetingApplication {
 
       memberRepository.save(adminMember);
 
+      for (int i=1; i<=5; i++) {
+        Product product = Product.builder()
+            .productName("상품 " + i)
+            .price(10000 + 1000*i)
+            .sellStatus(SellStatus.SELL)
+            .stockNumber(10 + 2*i)
+            .productDetail(i+ "번 상품입니다.")
+            .build();
+        productRepository.save(product);
+
+        ProductImage productImage = ProductImage.builder()
+            .product(product)
+            .imageUrl("/img/image" + i +".jpg")
+            .mainImageYn("Y")
+            .build();
+        productImageRepository.save(productImage);
+      }
     };
   }
 
