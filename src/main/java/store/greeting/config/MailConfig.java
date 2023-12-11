1bc689a61a324528ad8cc192091a82c6afbc7c89
EUIJOON <euijoonism@gmail.com>
2023-12-12 06:26:27 +0900

    [refactor] 네이버 메일 코드 우선 주석 처리

--- src/main/java/store/greeting/config/MailConfig.java
+++ src/main/java/store/greeting/config/MailConfig.java
@@ -1,44 +1,48 @@
 package store.greeting.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
-
-@Configuration
-public class MailConfig {
-
-    @Value("${naverId}")
-    private String naverId;
-
-    @Value("${naverPasswd}")
-    private String naverPasswd;
-
-    @Bean
-    public JavaMailSender javaMailService(){
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setHost("smtp.naver.com");
-        javaMailSender.setUsername(naverId);
-        javaMailSender.setPassword(naverPasswd);
-        javaMailSender.setPort(465);
-        javaMailSender.setJavaMailProperties(getMailProperties());
-
-        return javaMailSender;
-    }
-
-
-
-    private Properties getMailProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("mail.transport.protocol", "smtp");
-        properties.setProperty("mail.smtp.auth", "true");
-        properties.setProperty("mail.smtp.starttls.enable","true");
-        properties.setProperty("mail.debug","true");
-        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
-        properties.setProperty("mail.smtp.ssl.enable","true");
-        return properties;
-    }
-}
+//@Configuration
+//public class MailConfig {
+//
+////    @Value("${naverId}")
+////    private String naverId;
+////
+////    @Value("${naverPasswd}")
+////    private String naverPasswd;
+////
+////    @Bean
+////    public JavaMailSender javaMailService(){
+////        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+////        javaMailSender.setHost("smtp.naver.com");
+////        javaMailSender.setUsername(naverId);
+////        javaMailSender.setPassword(naverPasswd);
+////        javaMailSender.setPort(465);
+////        javaMailSender.setProtocol("smtps"); // SSL 사용
+////        javaMailSender.setJavaMailProperties(getMailProperties());
+////
+////        return javaMailSender;
+////    }
+////
+////    private Properties getMailProperties() {
+////        Properties properties = new Properties();
+////        properties.setProperty("mail.smtp.auth", "true");
+////        properties.setProperty("mail.smtp.starttls.enable", "true");
+////        properties.setProperty("mail.smtp.ssl.trust", "smtp.naver.com");
+////        properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+////        properties.setProperty("mail.smtp.socketFactory.fallback", "false");
+////        properties.setProperty("mail.smtp.socketFactory.port", "465");
+////
+////        return properties;
+////    }
+////
+////
+//////    private Properties getMailProperties() {
+//////        Properties properties = new Properties();
+//////        properties.setProperty("mail.transport.protocol", "smtp");
+//////        properties.setProperty("mail.smtp.auth", "true");
+//////        properties.setProperty("mail.smtp.starttls.enable","true");
+//////        properties.setProperty("mail.debug","true");
+//////        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
+//////        properties.setProperty("mail.smtp.ssl.enable","true");
+//////        return properties;
+//////    }
+//}
